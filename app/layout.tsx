910b54a65aa2cbf070d9e1ef99ed7dc0b41c8dae
Indranil <indranil@vyomainnovusglobal.com>
2025-09-18 10:36:55 +0530

    Update UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Sabar Puja',
+  description: 'West Bengal Police',
+
 }
 
 export default function RootLayout({
